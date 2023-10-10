c509db94d63f42bdc1c2dc5e0ad910e6aa5b1661
hulingyue <511809125@qq.com>
2023-10-10 16:44:15 +0800

    [add] compile.sh

---
+++ compile.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+while getopts ":p:" opt; do
+    case $opt in
+        p)
+            project="$OPTARG"
+            ;;
+        \?)
+            echo "无效的选项: -$OPTARG" >&2
+            ;;
+    esac
+done
+
+# log
+info() {
+    echo -- "\e[32m$1\e[0m\n" 
+}
+
+error() {
+    echo -- "\e[31mERROR: $1\e[0m"
+}
+
+FILE_NAME="$0"
+PROJECT_NAME="$project"
+
+# download third-party libraries
+git submodule update --init
+
+# compile
+if [ -d "$PROJECT_NAME" ]; then
+    cd "$PROJECT_NAME"
+    if [ ! -d build ]; then
+        mkdir build
+    fi
+    cd build
+    cmake ..
+    make -j16
+    info "COMMPILE SUCCESS!"
+else
+    error "UNKNOW PROJECT!"
+fi
