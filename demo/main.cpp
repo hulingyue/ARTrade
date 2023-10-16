63167c0c58d1a4f75bd341dc2de0adc9ef0552ce
hulingyue <511809125@qq.com>
2023-10-16 10:32:30 +0800

    [remove] remove boost

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
 #include <spdlog/spdlog.h>
-#include "test.h"
+#include "websocket.h"
 
 
 int main() {
+    // core::WebSocket::WebSocket *client = new core::WebSocket::WebSocket_Client();
+    // client->init("localhost", 8080);
     spdlog::info("hello world!");
     return 0;
 }
