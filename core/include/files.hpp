3fa3f04682087f3c7a4e084bdeada6b9d2caf6aa
hulingyue <511809125@qq.com>
2023-10-22 08:45:50 +0800

    [add] files

---
+++ core/include/files.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+#include <filesystem>
+
+#include <spdlog/spdlog.h>
+
+namespace CORE {
+namespace Private {
+
+bool create_folder(const std::string path, const std::string name) {
+    auto folder_path = std::filesystem::path(path) / std::filesystem::path(name);
+    try {
+        std::filesystem::create_directory(folder_path);
+        spdlog::info("create directory  - success!");
+        return true;
+    } catch (const std::filesystem::filesystem_error& e) {
+        spdlog::error("create direcroty - failure! msg: {}", e.what());
+    }
+    return false;
+}
+
+} // namespace FILES
+} // namespace CORE
