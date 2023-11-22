73fa2cfa42169be9b7da1b2c02dff4ab610e5cb5
hulingyue <511809125@qq.com>
2023-11-22 21:23:22 +0800

    [add] httpClient event

--- core/include/core/http_client.h
+++ core/include/core/http_client.h
@@ -10,6 +10,18 @@ public:
     HttpClient();
     virtual ~HttpClient();
 
+public:
+    HttpClient set_protocol(std::string protocol);
+    HttpClient set_host(std::string host);
+    HttpClient set_port(int port);
+    HttpClient set_header(httplib::Headers headers);
+
+    httplib::Result get(std::string path);
+    httplib::Result post(std::string path, httplib::Params params);
+
+private:
+    std::string generate_uri(std::string path);
+
 private:
     Self &self;
 };
--- core/src/http_client.cpp
+++ core/src/http_client.cpp
@@ -1,8 +1,15 @@
+#include <filesystem>
 #include "core/http_client.h"
 
 
 namespace {
 struct Self {
+    std::string protocol;
+    std::string host;
+    int port = 80;
+    httplib::Headers headers;
+
+    httplib::Client* client = nullptr;
 };
 }
 
@@ -14,7 +21,48 @@ HttpClient::HttpClient() : self( *new Self {} ) {
 }
 
 HttpClient::~HttpClient() {
-    delete &self;
+    if (self.client) { delete self.client; }
+    // if (&self) delete &self;
+}
+
+HttpClient HttpClient::set_protocol(std::string protocol) {
+    self.protocol = protocol;
+    return *this;
+}
+
+HttpClient HttpClient::set_host(std::string host) {
+    self.host = host;
+    return *this;
+}
+
+HttpClient HttpClient::set_port(int port) {
+    self.port = port;
+    return *this;
+}
+
+HttpClient HttpClient::set_header(httplib::Headers headers) {
+    self.headers = headers;
+    return *this;
+}
+
+httplib::Result HttpClient::get(std::string path) {
+    if (self.client == nullptr) {
+        self.client = new httplib::Client(self.host, self.port);
+    }
+
+    return self.client->Get(generate_uri(path), self.headers);
+}
+
+httplib::Result HttpClient::post(std::string path, httplib::Params params) {
+    if (self.client == nullptr) {
+        self.client = new httplib::Client(self.host, self.port);
+    }
+
+    return self.client->Post(generate_uri(path), self.headers, params);
+}
+
+inline std::string HttpClient::generate_uri(std::string path) {
+    return (std::filesystem::path(self.protocol) / self.host / path).string();
 }
 
 } // core::http::client
