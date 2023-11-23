c78e475b7725dcc8486bf874bdcb4830b870f758
hulingyue <511809125@qq.com>
2023-11-23 08:06:30 +0800

    [update] init trade

---
+++ bybit/src/bybit_trade.cpp
@@ -0,0 +1,53 @@
+#include "bybit_trade.h"
+
+
+namespace {
+struct Self {
+    std::atomic<bool> is_ready = false;
+};
+}
+
+
+BybitTrade::BybitTrade() : self ( *new Self {} ) {
+
+}
+
+BybitTrade::~BybitTrade() {
+    delete &self;
+}
+
+void BybitTrade::init() {
+
+}
+
+bool BybitTrade::is_ready() {
+    return self.is_ready.load();
+}
+
+TradeOperateResult BybitTrade::order(core::datas::OrderObj const &order) {
+
+}
+
+TradeOperateResult BybitTrade::cancel(core::datas::CancelObj const &order) {
+
+}
+
+void BybitTrade::interval_1s() {
+
+}
+
+void BybitTrade::on_open() {
+    self.is_ready.store(true);
+}
+
+void BybitTrade::on_close() {
+    self.is_ready.store(false);
+}
+
+void BybitTrade::on_message(std::string const &msg) {
+
+}
+
+void BybitTrade::ping() {
+
+}
--- core/include/core/trade.h
+++ core/include/core/trade.h
@@ -18,10 +18,11 @@ class Trade {
         Trade();
         virtual ~Trade();
 
+        virtual void init() = 0;
         virtual bool is_ready() = 0;
 
-        virtual TradeOperateResult order() = 0;
-        virtual TradeOperateResult cancel() = 0;
+        virtual TradeOperateResult order(core::datas::OrderObj const &order) = 0;
+        virtual TradeOperateResult cancel(core::datas::CancelObj const &order) = 0;
 
         virtual void set_message(core::message::Message* messsage) final;
     
