1012baf12d3a8c431621fe36f62d807f2038bddd
hulingyue <511809125@qq.com>
2023-12-17 23:46:23 +0800

    [bugfix] bug: when is_writting is false, will core dump

--- core/include/core/message/message.hpp
+++ core/include/core/message/message.hpp
@@ -56,14 +56,16 @@ public:
             throw std::runtime_error(LOGHEAD + " unable to load buffer!");
         }
 
-        header = reinterpret_cast<Header *>(address);
-        header->data_size = size - HeaderSize;
-        header->data_front_address = address + HeaderSize;
-        header->data_tail_address = address + size;
-        header->data_earliest_addresss = header->data_front_address;
-        header->data_lastest_address = header->data_front_address;
-        header->data_cursor_address = header->data_front_address;
-        header->data_next_address = header->data_front_address;
+        if (is_writting) {
+            header = reinterpret_cast<Header *>(address);
+            header->data_size = size - HeaderSize;
+            header->data_front_address = address + HeaderSize;
+            header->data_tail_address = address + size;
+            header->data_earliest_addresss = header->data_front_address;
+            header->data_lastest_address = header->data_front_address;
+            header->data_cursor_address = header->data_front_address;
+            header->data_next_address = header->data_front_address;
+        }
     }
 
     virtual ~Channel() {
