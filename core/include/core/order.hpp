08bc42a4c2c38467ba514628183eb1bf74b4d9df
hulingyue <511809125@qq.com>
2024-01-01 11:35:00 +0800

    [update] change type & add 'next_client_id'

--- core/include/core/order.hpp
+++ core/include/core/order.hpp
@@ -33,31 +33,42 @@ public:
         return instance.load();
     }
 
-    inline bool exist(const int client_id) { return (map_orders.find(client_id) != map_orders.end()); }
+    inline bool exist(const uint64_t client_id) { return (map_orders.find(client_id) != map_orders.end()); }
     
-    inline void insert_or_update(const int client_id, const command_pair_type command_pair) { map_orders[client_id] = command_pair; }
+    inline void insert_or_update(const uint64_t client_id, const command_pair_type command_pair) {
+        map_orders[client_id] = command_pair;
+        if (client_id > client_id_index.load()) {
+            client_id_index.store(client_id);
+        }
+    }
     
-    inline bool insert(const int client_id, const command_pair_type command_pair) {
+    inline bool insert(const uint64_t client_id, const command_pair_type command_pair) {
         if (exist(client_id)) { return false; }
         map_orders[client_id] = command_pair;
+        if (client_id > client_id_index.load()) {
+            client_id_index.store(client_id);
+        }
         return true;
     }
     
-    inline bool update(const int client_id, const command_pair_type command_pair) {
+    inline bool update(const uint64_t client_id, const command_pair_type command_pair) {
         if (!exist(client_id)) { return false; }
         map_orders[client_id] = command_pair;
         return true;
      }
     
-    inline bool remove(const int client_id) { return (map_orders.erase(client_id) > 0); }
+    inline bool remove(const uint64_t client_id) { return (map_orders.erase(client_id) > 0); }
     
     inline bool clean() {
         map_orders.clear();
         return map_orders.empty();
     }
 
+    inline uint64_t next_client_id() { return (client_id_index.load() + 1) % UINT64_MAX; }
+
 private:
-    std::unordered_map<int, command_pair_type> map_orders;
+    std::unordered_map<uint64_t, command_pair_type> map_orders;
+    std::atomic<uint64_t> client_id_index = 1;
 };
 } // namespace core::order
 
