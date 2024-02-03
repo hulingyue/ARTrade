d5a8020c21308298be2660c4df1aa598d767e562
hulingyue <511809125@qq.com>
2024-02-03 11:23:28 +0800

    [update] __init__.py

--- strategy/PythonApi/pystrategy/__init__.py
+++ strategy/PythonApi/pystrategy/__init__.py
@@ -1,9 +1,9 @@
-from .pystrategy import Strategy
-from .pystrategy import MarketOperateResult,\
+from pystrategy import Strategy
+from pystrategy import MarketOperateResult,\
     TradeOperateResult, TradePair, MessageType, \
     Identity, CommandStatus, OrderSide, OrderOffset, \
     OrderType, OrderTIF, OrderStatus
-from .pystrategy import MarketType, Market_base, Market_bbo, \
+from pystrategy import MarketType, Market_base, Market_bbo, \
     Market_depth, Market_kline
-from .pystrategy import CommandType, Command_base, SymbolObj, \
+from pystrategy import CommandType, Command_base, SymbolObj, \
     OrderObj, CancelObj
