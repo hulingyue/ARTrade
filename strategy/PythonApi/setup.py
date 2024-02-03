3e85132b7cf78c427764457100de62e234e95558
hulingyue <511809125@qq.com>
2024-02-03 10:53:24 +0800

    [update] new version

--- strategy/PythonApi/setup.py
+++ strategy/PythonApi/setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup, Extension
+from setuptools import setup
 
 setup(
     name='pystrategy',
-    version='1.0',
+    version="1.0.1",
     description="The python api for ARTrade's Strategy",
     url='https://github.com/hulingyue/ARTrade',
     license='MIT',
