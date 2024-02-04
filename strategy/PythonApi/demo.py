c581d82c083e24f478927998f34184aecbba32b0
hulingyue <511809125@qq.com>
2024-02-04 23:41:45 +0800

    [update] new demo only for order

--- strategy/PythonApi/demo.py
+++ strategy/PythonApi/demo.py
@@ -1,4 +1,5 @@
 from pystrategy import *
+import json
 
 
 class MyStrategy(Strategy):
@@ -10,20 +11,55 @@ class MyStrategy(Strategy):
 
         super().set_task(lambda: self.task())
         super().set_on_market_bbo(lambda bbo: self.on_market_bbo(bbo))
+        super().set_on_order(lambda obj: self.on_order(obj))
     
     def task(self):
-        obj = SymbolObj()
-        obj.command_type = CommandType.UNKNOW
-        obj.push_back("tickers.BTCUSDT")
-        obj.push_back("tickers.ETHUSDT")
+        # obj = SymbolObj()
+        # obj.command_type = CommandType.UNKNOW
+        # obj.push_back("tickers.BTCUSDT")
+        # obj.push_back("tickers.ETHUSDT")
 
-        self.subscribe(obj)
+        # self.subscribe(obj)
+
+        obj = OrderObj()
+        obj.status = OrderStatus.INIT
+        obj.command_type = CommandType.ORDER
+        obj.symbol = "BTCUSDT"
+        obj.exchange = "Bybit"
+        obj.type = OrderType.MARKET
+        obj.side = OrderSide.BUY
+        obj.offset = OrderOffset.OPEN
+        obj.tif = OrderTIF.GTC
+        obj.client_id = 0
+        obj.price = 40000
+        obj.quantity = 0.001
+        self.order(obj)
         while True:
             pass
 
     def on_market_bbo(self, bbo):
-        print(bbo.exchange, bbo.symbol, bbo.time, bbo.price)
+        # print(bbo.exchange, bbo.symbol, bbo.time, bbo.price)
+        pass
 
+    def on_order(self, obj: OrderObj):
+        message = json.dumps({
+            "symbol": obj.symbol,
+            "exchange": obj.exchange,
+            "msg": obj.msg,
+            "side": str(obj.side),
+            "offset": str(obj.offset),
+            "type": str(obj.type),
+            "status": str(obj.status),
+            "tif": str(obj.tif),
+            "client_id": obj.client_id,
+            "order_id": obj.order_id,
+            "price": obj.price,
+            "quantity": obj.quantity,
+            "traded": obj.traded,
+            "revoked": obj.revoked
+        })
+        print(message)
+        pass
 
 if __name__ == "__main__":
     t = MyStrategy()
