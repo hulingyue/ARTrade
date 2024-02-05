84d9835ee351f216ddd040357891ec8a3ab6b472
hulingyue <511809125@qq.com>
2024-02-05 20:40:47 +0800

    [update] save client_id as std::string

--- core/include/core/order.hpp
+++ core/include/core/order.hpp
@@ -32,27 +32,27 @@ public:
     }
 
     inline bool exist(const uint64_t client_id) {
-        return (map_orders.find(client_id) != map_orders.end());
+        return (map_orders.find(std::to_string(client_id)) != map_orders.end());
     }
     
     inline void insert_or_update(const uint64_t client_id, core::datas::OrderObj* obj) {
-        map_orders[client_id] = obj;
+        map_orders[std::to_string(client_id)] = obj;
     }
     
     inline bool insert(const uint64_t client_id, core::datas::OrderObj* obj) {
         if (exist(client_id)) { return false; }
-        map_orders[client_id] = obj;
+        map_orders[std::to_string(client_id)] = obj;
         return true;
     }
     
     inline bool update(const uint64_t client_id, core::datas::OrderObj* obj) {
         if (!exist(client_id)) { return false; }
-        map_orders[client_id] = obj;
+        map_orders[std::to_string(client_id)] = obj;
         return true;
      }
     
     inline bool remove(const uint64_t client_id) {
-        return (map_orders.erase(client_id) > 0);
+        return (map_orders.erase(std::to_string(client_id)) > 0);
     }
     
     inline bool clean() {
@@ -60,12 +60,16 @@ public:
         return map_orders.empty();
     }
 
-    core::datas::OrderObj* find(const uint64_t client_id) {
+    core::datas::OrderObj* find(const std::string client_id) {
         auto iterator = map_orders.find(client_id);
         if (iterator == map_orders.end()) {
             return nullptr;
         }
         return iterator->second;
+    }
+
+    core::datas::OrderObj* find(const uint64_t client_id) {
+        return find(std::to_string(client_id));
     }
 
     inline uint64_t next_client_id() {
@@ -75,7 +79,7 @@ public:
     }
 
 private:
-    std::unordered_map<uint64_t, core::datas::OrderObj*> map_orders;
+    std::unordered_map<std::string, core::datas::OrderObj*> map_orders;
     std::atomic<uint64_t> client_id_index = 1;
 };
 } // namespace core::order
