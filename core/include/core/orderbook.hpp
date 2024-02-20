599e4be4e1be45543dda7dbb3b1ecbe16328ca4c
hulingyue <511809125@qq.com>
2024-02-20 15:02:28 +0800

    [bugfix] orderbook

--- core/include/core/orderbook.hpp
+++ core/include/core/orderbook.hpp
@@ -6,112 +6,155 @@
 namespace core::datas { struct TradePair; }
 
 namespace {
+constexpr int MULTIPLE = 1000000;
+constexpr double ACCURACY = 0.000001;
+
 enum class PriceOrder {
     ESC,  // ask
     DESC  // bid
 };
 
+template<PriceOrder order, int depth>
 class SingleOrderBook {
 public:
-    SingleOrderBook(int depth, double accuracy, PriceOrder order)
-        : _depth(depth), _accuracy(accuracy), _order(order) {
-        data = new core::datas::TradePair[depth];
-        std::memset(data, 0, sizeof(core::datas::TradePair) * depth);
+    SingleOrderBook() {
+        _prices = new core::datas::TradePair[depth];
+        std::memset(_prices, 0, depth * sizeof(core::datas::TradePair));
     }
 
     ~SingleOrderBook() {
-        delete[] data;
+        delete []_prices;
     }
 
-    void init(core::datas::TradePair pair[], int size) {
-        map_index.clear();
+    double best_price() noexcept {
+        return static_cast<double>((_prices[0].price * 1.0) / MULTIPLE);
+    }
 
-        for (int index = 0; index < std::min(size, _depth); index++) {
-            data[index] = pair[index];
-            map_index[pair[index].price] = index;
-        }
+    core::datas::TradePair best_pair() noexcept {
+        return _prices[0];
+    }
 
-        for (int index = std::min(size, _depth); index < _depth; index++) {
-            data[index] = {_accuracy, _accuracy};
+    core::datas::TradePair* get() noexcept {
+        return _prices;
+    }
+
+    void init(core::datas::TradePair pairs[], int size) {
+        // init
+        map.clear();
+        std::memset(_prices, 0, depth * sizeof(core::datas::TradePair));
+    
+        for (int index = 0; index < std::min(size, depth); index++) {
+            long price = static_cast<long>(pairs[index].price * MULTIPLE);
+            _prices[index].price = price;
+            _prices[index].quantity = pairs[index].quantity;
+            map[price] = index;
         }
     }
 
     void update(core::datas::TradePair pair) {
-        auto it = map_index.find(pair.price);
-        if (it != map_index.end()) {
-            data[map_index[pair.price]] = pair;
+        long price = static_cast<long>(pair.price * MULTIPLE);
+        auto it = map.find(price);
+
+        // exist
+        if (it != map.end()) {
+            _prices[it->second].quantity = pair.quantity;
             return;
         }
 
-        // find & move
-        if (_order == PriceOrder::ESC) {
-            for (int index = 0; index < _depth; index++) {
-                if (data[index].price >= pair.price) {
-                    std::copy(data + index, data + _depth - 1, data + index + 1);
-                    data[index] = pair;
-                    break;
-                }
-            }
-        } else if (_order == PriceOrder::DESC) {
-            for (int index = 0; index < _depth; index++) {
-                if (data[index].price <= pair.price) {
-                    std::copy(data + index, data + _depth - 1, data + index + 1);
-                    data[index] = pair;
-                    break;
-                }
-            }
+        if (pair.quantity > ACCURACY) {
+            push_order(std::move(pair));
+        } else if (pair.quantity < ACCURACY) {
+            pop_order(std::move(pair));
         } else {
-            assert(false && "unknown PriceOrder!");
+
         }
     }
 
-    void update(double price, double quantity) {
-        update({.price = price, .quantity = quantity});
+    void push_order(core::datas::TradePair pair) {
+        long price = static_cast<long>(pair.price * MULTIPLE);
+        auto it = map.find(price);
+
+        // exist
+        if (it != map.end()) {
+            it->second += pair.quantity;
+            return;
+        }
+
+        // not exist
+        for (int index = 0; index < depth; index++) {
+            if ((order == PriceOrder::ESC && price < _prices[index].price) || (order == PriceOrder::DESC && price > _prices[index].price)) {
+                memcpy(_prices + index + 1, _prices + index, (depth - index - 1) * sizeof(core::datas::TradePair));
+                _prices[index].price = price;
+                _prices[index].quantity = pair.quantity;
+                map[price] = index;
+                return;
+            }
+        }
     }
 
-    core::datas::TradePair* get() {
-        return data;
+    void pop_order(core::datas::TradePair pair) {
+        long price = static_cast<long>(pair.price * MULTIPLE);
+        auto it = map.find(price);
+
+        // not exist
+        if (it == map.end()) {
+            return;
+        }
+
+        // exist
+        it->second -= pair.quantity;
+        if (it->second > ACCURACY) {
+            return;
+        }
+
+        map.erase(it);
+        for (int index = 0; index < depth; index++) {
+            if (price == _prices[index].price) {
+                memcpy(_prices + index, _prices + index + 1, (depth - index - 1) * sizeof(core::datas::TradePair));
+                if (order == PriceOrder::ESC) {
+                    _prices[depth - 1].price = INTMAX_MAX;
+                } else if (order == PriceOrder::DESC) {
+                    _prices[depth - 1].price = INTMAX_MIN;
+                }
+                return;
+            }
+        }
     }
 
 private:
-    int _depth;
-    double _accuracy;
-    PriceOrder _order;
-
-    // price -> location
-    std::unordered_map<double, int> map_index;
-    core::datas::TradePair *data;
+    // key: price value: location
+    std::unordered_map<long, int> map;
+    // price queue
+    core::datas::TradePair* _prices;
 };
 } // namespace
 
 namespace core::orderbook {
 class OrderBook {
 public:
-    OrderBook(int depth, double accuracy=0.00001) {
-        asks = new SingleOrderBook(depth, accuracy, PriceOrder::ESC);
-        bids = new SingleOrderBook(depth, accuracy, PriceOrder::DESC);
-    }
+    double best_ask() noexcept { return _ask.best_price(); }
+    double best_bid() noexcept { return _bid.best_price(); }
 
-    ~OrderBook() {
-        delete asks;
-        delete bids;
-    }
+    core::datas::TradePair best_ask_pair() noexcept { return _ask.best_pair(); }
+    core::datas::TradePair best_bid_pair() noexcept { return _bid.best_pair(); }
 
-    void init_asks(core::datas::TradePair pair[], int size) { asks->init(pair, size); }
-    void init_bids(core::datas::TradePair pair[], int size) { bids->init(pair, size); }
+    core::datas::TradePair* get_asks() noexcept { return _ask.get(); }
+    core::datas::TradePair* get_bids() noexcept { return _bid.get(); }
 
-    void update_asks(double price, double quantity) { asks->update(price, quantity); }
-    void update_bids(double price, double quantity) { bids->update(price, quantity); }
+    void init_ask(core::datas::TradePair pairs[], int size) { return _ask.init(pairs, size); }
+    void init_bid(core::datas::TradePair pairs[], int size) { return _bid.init(pairs, size); }
 
-    core::datas::TradePair* get_asks() { return asks->get(); }
-    core::datas::TradePair* get_bids() { return bids->get(); }
-    std::pair<core::datas::TradePair*, core::datas::TradePair*> get() {
-        return std::make_pair(asks->get(), bids->get());
-    }
+    void update_ask(core::datas::TradePair pair) { return _ask.update(std::move(pair)); }
+    void update_bid(core::datas::TradePair pair) { return _bid.update(std::move(pair)); }
 
+    void push_ask_order(core::datas::TradePair pair) { return _ask.push_order(std::move(pair)); }
+    void push_bid_order(core::datas::TradePair pair) { return _bid.push_order(std::move(pair)); }
+
+    void pop_ask_order(core::datas::TradePair pair) { return _ask.pop_order(std::move(pair)); }
+    void pop_bid_order(core::datas::TradePair pair) { return _bid.pop_order(std::move(pair)); }
 private:
-    SingleOrderBook *asks;
-    SingleOrderBook *bids;
+    SingleOrderBook<PriceOrder::ESC, core::datas::MARKET_MAX_DEPTH> _ask;
+    SingleOrderBook<PriceOrder::DESC, core::datas::MARKET_MAX_DEPTH> _bid;
 };
 
 class OrderbookManager {
@@ -119,63 +162,43 @@ public:
     OrderbookManager() = default;
     ~OrderbookManager() = default;
 
-    void init_asks(std::string symbol, core::datas::TradePair pair[], int size, double accuracy=0.00001) {
-        // key_exist_or_create(symbol, size, accuracy);
-        auto orderbook = _map[symbol][size];
-        orderbook->init_asks(pair, size);
+    void init_asks(std::string symbol, core::datas::TradePair pair[], int size) {
+        return _map[symbol]->init_ask(pair, size);
     }
 
-    void init_bids(std::string symbol, core::datas::TradePair pair[], int size, double accuracy=0.00001) {
-        // key_exist_or_create(symbol, size, accuracy);
-        auto orderbook = _map[symbol][size];
-        orderbook->init_bids(pair, size);
+    void init_bids(std::string symbol, core::datas::TradePair pair[], int size) {
+        return _map[symbol]->init_bid(pair, size);
     }
 
-    void update_asks(std::string symbol, int depth, double price, double quantity, double accuracy=0.00001) {
-        // key_exist_or_create(symbol, depth, accuracy);
-        auto orderbook = _map[symbol][depth];
-        orderbook->update_asks(price, quantity);
+    void update_asks(std::string symbol, int depth, double price, double quantity) {
+        return _map[symbol]->update_ask({price, quantity});
     }
 
-    void update_bids(std::string symbol, int depth, double price, double quantity, double accuracy=0.00001) {
-        // key_exist_or_create(symbol, depth, accuracy);
-        auto orderbook = _map[symbol][depth];
-        orderbook->update_bids(price, quantity);
+    void update_bids(std::string symbol, int depth, double price, double quantity) {
+        return _map[symbol]->update_bid({price, quantity});
     }
 
-    core::datas::TradePair* get_asks(std::string symbol, int depth, double accuracy=0.00001) {
-        // key_exist_or_create(symbol, depth, accuracy);
-        auto orderbook = _map[symbol][depth];
-        return orderbook->get_asks();
+    core::datas::TradePair* get_asks(std::string symbol, int depth) {
+        return _map[symbol]->get_asks();
     }
 
-    core::datas::TradePair* get_bids(std::string symbol, int depth, double accuracy=0.00001) {
-        // key_exist_or_create(symbol, depth, accuracy);
-        auto orderbook = _map[symbol][depth];
-        return orderbook->get_bids();
+    core::datas::TradePair* get_bids(std::string symbol, int depth) {
+        return _map[symbol]->get_bids();
     }
 
-    std::pair<core::datas::TradePair*, core::datas::TradePair*> get(std::string symbol, int depth, double accuracy=0.00001) {
-        // key_exist_or_create(symbol, depth, accuracy);
-        // auto orderbook = _map[symbol][depth];
-        return std::make_pair(get_asks(symbol, depth, accuracy), get_bids(symbol, depth, accuracy));
+    std::pair<core::datas::TradePair*, core::datas::TradePair*> get(std::string symbol, int depth) {
+        return std::make_pair(get_asks(symbol, depth), get_bids(symbol, depth));
     }
 
-// private:
     void key_exist_or_create(std::string symbol, int depth, double accuracy) {
         auto it = _map.find(symbol);
         if (it == _map.end()) {
-            _map[symbol] = std::unordered_map<int, OrderBook*>();
-        }
-
-        auto inner_it = _map[symbol].find(depth);
-        if (inner_it == _map[symbol].end()) {
-            _map[symbol][depth] = new OrderBook(depth, accuracy);
+            _map[symbol] = new OrderBook();
         }
     }
 
 private:
     // symbol<depth, info>
-    std::unordered_map<std::string, std::unordered_map<int, OrderBook*>> _map;
+    std::unordered_map<std::string, OrderBook*> _map;
 };
 } // namespace core::orderbook
